a30ee40c974088d5c9471a124af554465544eaea
Akashiutchiha <christianhonore2003@gmail.com>
2023-03-31 22:10:41 +0100

    init: Building a new app called Funapp

--- flashchat/lib/main.dart
+++ flashchat/lib/main.dart
@@ -1,13 +1,6 @@
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'screens/chat_screen.dart';
-import 'screens/welcome_screen.dart';
-import 'screens/login_screen.dart';
-import 'screens/registration_screen.dart';
 
 Future<void> main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
   runApp(FlashChat());
 }
 
@@ -20,13 +13,16 @@ class FlashChat extends StatelessWidget {
           bodyLarge: TextStyle(color: Colors.black54),
         ),
       ),
-      initialRoute: WelcomeScreen.welcome,
-      routes: {
-        WelcomeScreen.welcome: (context) => WelcomeScreen(),
-        ChatScreen.chat: (context) => ChatScreen(),
-        RegistrationScreen.registration: (context) => RegistrationScreen(),
-        LoginScreen.login: (context) => LoginScreen()
-      },
+      home: SafeArea(
+        child: Scaffold(
+          appBar: AppBar(
+            title: Text("Fun App"),
+          ),
+          body: Center(
+            child: Text("Default Text"),
+          ),
+        ),
+      ),
     );
   }
 }
