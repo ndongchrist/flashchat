2ca4e9db6e53a7a2514c4c895ec2b7817b9a7c37
Akashiutchiha <christianhonore2003@gmail.com>
2023-05-03 22:55:10 +0100

    fix: Fixed all the page navigations

--- flutter_music_app/lib/widgets/platlist_card.dart
+++ flutter_music_app/lib/widgets/platlist_card.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import '../models/playlist_model.dart';
+import '../screens/playlist_screen.dart';
 
 class PlaylistCard extends StatelessWidget {
   const PlaylistCard({
@@ -21,22 +22,29 @@ class PlaylistCard extends StatelessWidget {
             scrollDirection: Axis.vertical,
             itemCount: playlists.length,
             itemBuilder: (context, index) {
-              return Container(
-                margin: EdgeInsets.only(bottom: 15),
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(15),
-                  color: Colors.deepPurple.shade600,
-                ),
-                child: ListTile(
-                  trailing: Icon(
-                    Icons.play_circle,
-                    color: Colors.white,
+              return GestureDetector(
+                onTap: () {
+                  Navigator.push(context, MaterialPageRoute(builder: (context) {
+                    return PlaylistScreen();
+                  }));
+                },
+                child: Container(
+                  margin: EdgeInsets.only(bottom: 15),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(15),
+                    color: Colors.deepPurple.shade600,
                   ),
-                  leading: CircleAvatar(
-                    backgroundImage: AssetImage('assets/album_pic.jpg'),
+                  child: ListTile(
+                    trailing: Icon(
+                      Icons.play_circle,
+                      color: Colors.white,
+                    ),
+                    leading: CircleAvatar(
+                      backgroundImage: AssetImage('assets/album_pic.jpg'),
+                    ),
+                    title: Text(playlists[index].title),
+                    subtitle: Text(playlists[index].songs.length.toString()),
                   ),
-                  title: Text(playlists[index].title),
-                  subtitle: Text(playlists[index].songs.length.toString()),
                 ),
               );
             },
